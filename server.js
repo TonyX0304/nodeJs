19cead33988f0b04cd84e79bec466bb1fbee076b
肖甜 <tony-x@yungehuo.com>
2020-12-13 17:35:13 +0800

    test

--- server.js
+++ server.js
@@ -33,20 +33,20 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 //  console.log(`server running @${app.get('port')}`);
 // })
 // //设置跨域访问
-// app.all("*",function(req,res,next){
-//   console.log(req.path)
-//   console.log(req.method)
-//   //设置允许跨域的域名，*代表允许任意域名跨域
-//   res.header("Access-Control-Allow-Origin","*");
-//   //允许的header类型
-//   res.header("Access-Control-Allow-Headers","content-type");
-//   //跨域允许的请求方式 
-//   res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
-//   if (req.method.toLowerCase() == 'options')
-//     res.send(200);  //让options尝试请求快速结束
-//   else
-//     next();
-// })
+app.all("*",function(req,res,next){
+  console.log(req)
+  console.log(req.method)
+  //设置允许跨域的域名，*代表允许任意域名跨域
+  res.header("Access-Control-Allow-Origin","*");
+  //允许的header类型
+  res.header("Access-Control-Allow-Headers","content-type");
+  //跨域允许的请求方式 
+  res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
+  if (req.method.toLowerCase() == 'options')
+    res.send(200);  //让options尝试请求快速结束
+  else
+    next();
+})
 
 app.get('/find', function(req, res){
   db.find('students', {}, function(err, result) {
@@ -103,23 +103,23 @@ app.get(/mongo\S*/, function(req, res){
 })
 
 // app.get(/api\S*/, function(req, res){
-// app.get(/api\S*/, function(req, res){
-//   let originalUrl = req.originalUrl
-//   let method = req.method.toLowerCase()
-//   let protocol = req.protocol
-//   let query = req.query
-//   let params = req.params
-//   let isXhr = req.xhr
-//   let r = {
-//     method,
-//     protocol,
-//     originalUrl,
-//     query,
-//     params,
-//     isXhr
-//   }
-//   res.json(new Result({data: {name: '小米', en: 'mi', ...r, ...req['headers']}}))
-// })
+app.get(/api\S*/, function(req, res){
+  let originalUrl = req.originalUrl
+  let method = req.method.toLowerCase()
+  let protocol = req.protocol
+  let query = req.query
+  let params = req.params
+  let isXhr = req.xhr
+  let r = {
+    method,
+    protocol,
+    originalUrl,
+    query,
+    params,
+    isXhr
+  }
+  res.json(new Result({data: {name: '小米', en: 'mi', ...r, ...req['headers']}}))
+})
 
 app.get('/api/folder', file.getAllFolder)
 
